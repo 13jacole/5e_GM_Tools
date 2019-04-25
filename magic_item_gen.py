77660bba1371c38337f182a25389c9a6d98305c7
13jacole <jakobcole13@gmail.com>
2019-04-25 13:33:27 -0400

    Create magic_item_gen.py

---
+++ magic_item_gen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#dependencies
+import os
+import sys
+import argparse
+import warnings
+
+#more imports
+
+"""
+magic_item_gen.py: Random amgic item selection program
+       Usage: python magic_item_gen.py ????
+"""

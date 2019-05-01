f7bab23d7ca3c08ecd7c3043e94b4ab07931c1fb
13jacole <jakobcole13@gmail.com>
2019-04-30 20:31:26 -0400

    increased input control robust-ness

--- Magic_Item_Selector/magic_item_sel.py
+++ Magic_Item_Selector/magic_item_sel.py
@@ -12,7 +12,7 @@ import numpy as np
 #more imports
 
 """
-magic_item_gen.py: Random amgic item selection program
+magic_item_sel.py: Random magic item selection program
        Usage: python magic_item_sel.py -s major/minor -r common/uncommon/rare/veryrare/legendary -n number of items to be generated (optional -- Default 1) -o Name of output text file
 """
 
@@ -50,49 +50,68 @@ itemfile = str(item_scope) + '_' + str(item_rarity) + '.csv'
 my_data = pd.read_csv(itemfile, delimiter=',', header=0)
 
 #Determine item filter decision
-print('Do you want to filter by item type? (Y/N): ')
-dec = input()
+print('Do you want to filter by item type? (Y/N)')
+valid = False
+
+while valid == False:
+	dec = input()
+	if dec.lower() != 'y' and dec.lower() != 'yes' and dec.lower() != 'n' and dec.lower() != 'no':
+		print("I'm sorry. I do not understand your input. Please indicate Y or N\n\n")
+		print("Do you want to filter by item type? (Y/N)")
+	else:
+		valid = True
+
 
 if dec.lower() == 'y' or dec.lower == 'yes':
 	print('Using the following list, please select which item types you want to choose from\n')
 	print('Selection Number -- Item Type\n')
 	
+	totFilt = []
 	filt_data = my_data[my_data['Type'].isin(['Armor'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       1         -- Armor\n')
+		totFilt.append(1)
 	filt_data = my_data[my_data['Type'].isin(['Potion'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       2         -- Potion\n')
+		totFilt.append(2)
 	filt_data = my_data[my_data['Type'].isin(['Ring'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       3         -- Ring\n')
+		totFilt.append(3)
 	filt_data = my_data[my_data['Type'].isin(['Rod'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       4         -- Rod\n')
+		totFilt.append(4)
 	filt_data = my_data[my_data['Type'].isin(['Scroll'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       5         -- Scroll\n')
+		totFilt.append(5)
 	filt_data = my_data[my_data['Type'].isin(['Staff'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       6         -- Staff\n')
+		totFilt.append(6)
 	filt_data = my_data[my_data['Type'].isin(['Wand'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       7         -- Wand\n')
+		totFilt.append(7)
 	filt_data = my_data[my_data['Type'].isin(['Weapon'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       8         -- Weapon\n')
+		totFilt.append(8)
 	filt_data = my_data[my_data['Type'].isin(['Wondrous Item'])]
 	(rows, cols) = filt_data.shape
 	if rows > 0:
 		print('       9         -- Wondrous Item\n\n')
+		totFilt.append(9)
 	
 	print('Please use the number selection column.\n')
 	print('If you have more than one item type you want to pick from\n')
@@ -101,31 +120,31 @@ if dec.lower() == 'y' or dec.lower == 'yes':
 	typefilt = list(typefilt)
 	
 	typefiltname = []
-	if '1' in typefilt:
+	if '1' in typefilt and 1 in totFilt:
 		typefiltname.append('Armor')
 		print('You have selected Armor\n')
-	if '2' in typefilt:
+	if '2' in typefilt and 2 in totFilt:
 		typefiltname.append('Potion')
 		print('You have selected Potion\n')
-	if '3' in typefilt:
+	if '3' in typefilt and 3 in totFilt:
 		typefiltname.append('Ring')
 		print('You have selected Ring\n')
-	if '4' in typefilt:
+	if '4' in typefilt and 4 in totFilt:
 		typefiltname.append('Rod')
 		print('You have selected Rod\n')
-	if '5' in typefilt:
+	if '5' in typefilt and 5 in totFilt:
 		typefiltname.append('Scroll')
 		print('You have selected Scroll\n')
-	if '6' in typefilt:
+	if '6' in typefilt and 6 in totFilt:
 		typefiltname.append('Staff')
 		print('You have selected Staff\n')
-	if '7' in typefilt:
+	if '7' in typefilt and 7 in totFilt:
 		typefiltname.append('Wand')
 		print('You have selected Wand\n')
-	if '8' in typefilt:
+	if '8' in typefilt and 8 in totFilt:
 		typefiltname.append('Weapon')
 		print('You have selected Weapon\n')
-	if '9' in typefilt:
+	if '9' in typefilt and 9 in totFilt:
 		typefiltname.append('Wondrous item')
 		print('You have selected Wondrous item\n')
 	
@@ -134,6 +153,7 @@ else:
 	item_data = my_data
 
 f.write(item_scope + " " + item_rarity + "\n")
+
 while number > 0:
 	#pick random item
 	(rows, cols) = item_data.shape
@@ -142,7 +162,16 @@ while number > 0:
 	if item_data.iloc[x,3] != 'All':
 		print(item_data.iloc[x,0] + " has the following use restriction: " + item_data.iloc[x,3])
 		print("Do you want to generate a different item? (Y/N)")
-		Rest_Dec = input()
+		valid = False
+		
+		while valid == False:
+			Rest_Dec = input()
+			if Rest_Dec.lower() != 'y' and Rest_Dec.lower() != 'yes' and Rest_Dec.lower() != 'n' and Rest_Dec.lower() != 'no':
+				print("I'm sorry. I do not understand your input. Please indicate Y or N\n\n")
+				print("Do you want to generate a different item? (Y/N)")
+			else:
+				valid = True
+		
 	else:
 		Rest_Dec = 'n'
 	

1a2d7d0ffd10b39204457aa46944cc6477a6ce39
13jacole <jakobcole13@gmail.com>
2019-05-07 22:46:02 -0400

    Update music_play.py
    Added arguments, and started code for playlist vs song options

--- Music_Player/music_play.py
+++ Music_Player/music_play.py
@@ -5,28 +5,82 @@ import os
 import sys
 import argparse
 import glob
-import random
+import random as rando
 from playsound import playsound
 
-#Skeleton code
+"""
+music_play.py: Music Player
+       Usage: python music_play.py 
+"""
+
+# meta information
+__author__ = "Jakob Cole"
+__maintainer__ = "Jakob Cole"
+
+#arguments
+pa = argparse.ArgumentParser()
+pa.add_argument('-l', '--loop', action='store_true',
+				help='Flag specifies if music should loop')
+				
+pa.add_argument('-r', '--random', action='store_true',
+				help='Flag specifies whether music will be randomized or not')
+
+pa.add_argument('-p', '--playlist', action='store_true',
+				help='Flag specifies whether music will be playlist or song')
+
+args = pa.parse_args()	
+
+#argument parsing
+if args.loop:
+	loop = True
+else:
+	loop = False
+	
+if args.random:
+	random = True
+else:
+	random = False
+	
+if args.playlist:
+	playlist = True
+else:
+	playlist = False
+
+#skeleton code
+
 currDir = os.getcwd()
-print("What playlist do you want to play?\n")
+
+if playlist:
+	print("What playlist do you want to play?\n")
+	print("Please type the name of the subdirectory: \n")
+else:
+	print("From what playlist do you want to select your song?\n")
+	print("Please type the name of the subdirectory: \n")
+	
 decision = input()
 decision = decision.replace(" ", "_")
 newDir = currDir + "\\" + decision
-os.chdir(newDir)
 
 print(os.getcwd())
+
+os.chdir(newDir)
 
 mp3List = glob.glob1(newDir,"*.mp3")
 mp3Counter = len(mp3List)
 
-if mp3Counter > 1:
-	x = random.randint(0, (mp3Counter-1))
-elif mp3Counter == 1:
-	x = 1
+if playlist:
+	if mp3Counter > 1:
+		x = rando.randint(0, (mp3Counter-1))
+	elif mp3Counter == 1:
+		x = 1
+	else:
+		print("No MP3 files exist in this directory")
+		sys.exit()
+		
+	playsound(mp3List[x])
 else:
-	print("No MP3 files exist in this directory")
-	sys.exit()
-	
-playsound(mp3List[x])
+	print("\n")
+	print("Songs in this directory:")
+	print("\n")
+	for idx, song in enumerate(mp3List, start=1):
+		print("{}: {}\n".format(idx, song))
